592b8d8d4c415b37ac485cf48198666fd4d879c0
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2017-05-08 03:12:07 +0800

    Fix a regression bug caused by accidentally removed lock-protected ref-counting in Ime::Module.

--- ImeModule.cpp
+++ ImeModule.cpp
@@ -505,11 +505,11 @@ STDMETHODIMP_(ULONG) ImeModule::Release(void) {
 	// The last reference is released in DllMain() when unloading.
 	// Hence interlocked operations are enough here, I guess.
 	assert(refCount_ > 0);
-	const ULONG newCount = --refCount_;
 	if(::InterlockedExchangeSubtract(&refCount_, 1) == 1) {
 		delete this;
+		return 0;
 	}
-	return newCount;
+	return refCount_;
 }
 
 // IClassFactory
