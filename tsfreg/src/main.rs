6e0a328c8c98645bb594e51c4acb63183ba8ef64
Kan-Ru Chen <kanru@kanru.info>
2025-05-06 16:18:20 +0900

    feat: enable ChewingTIP by default
    Thanks to the information from mozc and keyman:
    
    * https://github.com/google/mozc/issues/736
    * https://github.com/keymanapp/keyman/wiki/Making-sense-of-the-Windows-Layout-Registration#installlayoutortip-win32-api

--- tsfreg/src/main.rs
+++ tsfreg/src/main.rs
@@ -12,8 +12,24 @@ use windows::{
 };
 use windows_registry::LOCAL_MACHINE;
 
+// https://learn.microsoft.com/en-us/windows/win32/tsf/installlayoutortip
+windows_link::link!("input.dll" "system" fn InstallLayoutOrTip(psz: *const u16, dwFlags: u32));
+const ILOT_INSTALL: u32 = 0x00000000;
+const ILOT_UNINSTALL: u32 = 0x00000001;
+
 const CHEWING_TSF_CLSID: GUID = GUID::from_u128(0x13F2EF08_575C_4D8C_88E0_F67BB8052B84);
 const CHEWING_PROFILE_GUID: GUID = GUID::from_u128(0xCE45F71D_CE79_41D1_967D_640B65A380E3);
+const CHEWING_TIP_DESC: PCWSTR =
+    w!("0x0404:{13F2EF08-575C-4D8C-88E0-F67BB8052B84}{CE45F71D-CE79-41D1-967D-640B65A380E3}");
+
+const CATEGORIES: [GUID; 6] = [
+    GUID_TFCAT_TIP_KEYBOARD,
+    GUID_TFCAT_DISPLAYATTRIBUTEPROVIDER,
+    GUID_TFCAT_TIPCAP_INPUTMODECOMPARTMENT,
+    GUID_TFCAT_TIPCAP_UIELEMENTENABLED,
+    GUID_TFCAT_TIPCAP_IMMERSIVESUPPORT,
+    GUID_TFCAT_TIPCAP_SYSTRAYSUPPORT,
+];
 
 fn register(icon_path: String) -> Result<()> {
     unsafe {
@@ -36,44 +52,15 @@ fn register(icon_path: String) -> Result<()> {
             0,
             HKL::default(),
             0,
-            true,
+            false,
             0,
         )?;
 
         let category_manager: ITfCategoryMgr =
             CoCreateInstance(&CLSID_TF_CategoryMgr, None, CLSCTX_INPROC_SERVER)?;
-
-        category_manager.RegisterCategory(
-            &CHEWING_TSF_CLSID,
-            &GUID_TFCAT_TIP_KEYBOARD,
-            &CHEWING_TSF_CLSID,
-        )?;
-        category_manager.RegisterCategory(
-            &CHEWING_TSF_CLSID,
-            &GUID_TFCAT_DISPLAYATTRIBUTEPROVIDER,
-            &CHEWING_TSF_CLSID,
-        )?;
-        category_manager.RegisterCategory(
-            &CHEWING_TSF_CLSID,
-            &GUID_TFCAT_TIPCAP_INPUTMODECOMPARTMENT,
-            &CHEWING_TSF_CLSID,
-        )?;
-        category_manager.RegisterCategory(
-            &CHEWING_TSF_CLSID,
-            &GUID_TFCAT_TIPCAP_UIELEMENTENABLED,
-            &CHEWING_TSF_CLSID,
-        )?;
-
-        category_manager.RegisterCategory(
-            &CHEWING_TSF_CLSID,
-            &GUID_TFCAT_TIPCAP_IMMERSIVESUPPORT,
-            &CHEWING_TSF_CLSID,
-        )?;
-        category_manager.RegisterCategory(
-            &CHEWING_TSF_CLSID,
-            &GUID_TFCAT_TIPCAP_SYSTRAYSUPPORT,
-            &CHEWING_TSF_CLSID,
-        )?;
+        for tfcat in &CATEGORIES {
+            category_manager.RegisterCategory(&CHEWING_TSF_CLSID, tfcat, &CHEWING_TSF_CLSID)?;
+        }
     }
 
     #[cfg(debug_assertions)]
@@ -93,6 +80,16 @@ fn unregister() -> Result<()> {
         let input_processor_profile_mgr: ITfInputProcessorProfileMgr =
             CoCreateInstance(&CLSID_TF_InputProcessorProfiles, None, CLSCTX_INPROC_SERVER)?;
 
+        let category_manager: ITfCategoryMgr =
+            CoCreateInstance(&CLSID_TF_CategoryMgr, None, CLSCTX_INPROC_SERVER)?;
+        for tfcat in &CATEGORIES {
+            if let Err(error) =
+                category_manager.UnregisterCategory(&CHEWING_TSF_CLSID, tfcat, &CHEWING_TSF_CLSID)
+            {
+                println!("Failed to unregister category {tfcat:?}: {error}");
+            }
+        }
+
         let mut lcid = LocaleNameToLCID(w!("zh-Hant-TW"), 0);
         if lcid == 0 {
             lcid = LocaleNameToLCID(w!("zh-TW"), 0);
@@ -108,6 +105,18 @@ fn unregister() -> Result<()> {
     Ok(())
 }
 
+fn enable() {
+    unsafe {
+        InstallLayoutOrTip(CHEWING_TIP_DESC.as_ptr(), ILOT_INSTALL);
+    }
+}
+
+fn disable() {
+    unsafe {
+        InstallLayoutOrTip(CHEWING_TIP_DESC.as_ptr(), ILOT_UNINSTALL);
+    }
+}
+
 fn main() -> Result<()> {
     unsafe {
         CoInitializeEx(None, COINIT_APARTMENTTHREADED).ok()?;
@@ -115,6 +124,8 @@ fn main() -> Result<()> {
         if env::args().len() == 1 {
             println!("Usage:");
             println!("  tsfreg -r <IconPath>    註冊輸入法");
+            println!("  tsfreg -i           立即啟用輸入法");
+            println!("  tsfreg -d           立即停用輸入法");
             println!("  tsfreg -u                 取消註冊");
             process::exit(1);
         }
@@ -122,6 +133,10 @@ fn main() -> Result<()> {
         if let Some("-r") = env::args().nth(1).as_ref().map(|s| s.as_str()) {
             let icon_path = env::args().nth(2).expect("缺少 IconPath");
             register(icon_path)?;
+        } else if let Some("-i") = env::args().nth(1).as_ref().map(|s| s.as_str()) {
+            enable();
+        } else if let Some("-d") = env::args().nth(1).as_ref().map(|s| s.as_str()) {
+            disable();
         } else {
             if let Err(err) = unregister() {
                 println!("警告：無法解除輸入法註冊，反安裝可能無法正常完成。");
