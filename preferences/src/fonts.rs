fec8b305929ca382d52f228b239d38e3bb4a709d
Kan-Ru Chen <kanru@kanru.info>
2025-07-13 15:12:21 +0900

    feat: display localized font names in preferences

--- preferences/src/fonts.rs
+++ preferences/src/fonts.rs
@@ -1,6 +1,11 @@
+use std::cmp::Ordering;
+
 use anyhow::Result;
 use slint::SharedString;
-use windows::Win32::Graphics::DirectWrite::*;
+use windows::{
+    Win32::{Foundation::FALSE, Graphics::DirectWrite::*},
+    core::w,
+};
 
 pub(super) fn enum_font_families() -> Result<Vec<SharedString>> {
     let factory: IDWriteFactory = unsafe { DWriteCreateFactory(DWRITE_FACTORY_TYPE_SHARED)? };
@@ -13,12 +18,28 @@ pub(super) fn enum_font_families() -> Result<Vec<SharedString>> {
             for i in 0..family_count {
                 let family = collection.GetFontFamily(i)?;
                 let names = family.GetFamilyNames()?;
-                let len = names.GetStringLength(0)?;
+                let mut index = 0;
+                let mut exists = FALSE;
+                names.FindLocaleName(w!("zh-tw"), &mut index, &mut exists)?;
+                if !exists.as_bool() {
+                    index = 0;
+                }
+                let len = names.GetStringLength(index)?;
                 let mut name = vec![0u16; (len + 1) as usize];
-                names.GetString(0, &mut name)?;
+                names.GetString(index, &mut name)?;
                 res.push(String::from_utf16_lossy(&name).into());
             }
         }
     }
+    res.sort_by(|a: &SharedString, b| {
+        let a_localized = a.chars().any(|ch| !ch.is_ascii());
+        let b_localized = b.chars().any(|ch| !ch.is_ascii());
+        match (a_localized, b_localized) {
+            (true, true) => a.cmp(b),
+            (true, false) => Ordering::Less,
+            (false, true) => Ordering::Greater,
+            (false, false) => a.cmp(b),
+        }
+    });
     Ok(res)
 }
