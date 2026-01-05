039e50e741cdc9234596c460ca79f295a2e06791
Kan-Ru Chen <kanru@kanru.info>
2026-01-05 17:41:43 +0900

    chore(release): bump version to 26.1.0.x

--- preferences/src/version.tsx
+++ preferences/src/version.tsx
@@ -1,5 +1,5 @@
-const Version = {
-    productVersion: "25.10.0.0",
-    buildDate: "2025 年 10 月 04 日",
+const version = {
+    productVersion: "26.1.0.0",
+    buildDate: "2026 年 01 月 05 日",
 };
-export default Version;
+export default version;
