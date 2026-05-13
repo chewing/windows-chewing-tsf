b24a7d0f7c6d007536a997187fb9c96939af99d7
Kan-Ru Chen <kanru@kanru.info>
2026-05-14 07:06:27 +0900

    tip_host: embed product version metadata

--- crates/chewing_tip_host/build.rs
+++ crates/chewing_tip_host/build.rs
@@ -4,6 +4,14 @@
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    embed_resource::compile("rc/chewing_tip_host.rc", embed_resource::NONE).manifest_required()?;
+    // embed_resource::compile("rc/chewing_tip_host.rc", embed_resource::NONE).manifest_required()?;
+    embedinator::ResourceBuilder::from_env()
+        .add_manifest(std::fs::read_to_string("rc/chewing_tip_host.manifest").unwrap())
+        .add_string(
+            "LegalCopyright",
+            "Copyright (C) 2013-2026 libchewing Core Team",
+        )
+        .finish();
+    println!("cargo:rerun-if-changed=app.manifest");
     Ok(())
 }
