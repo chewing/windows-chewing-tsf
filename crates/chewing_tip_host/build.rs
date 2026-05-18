df1c847d1d4a297e8e62faca7fc569b93d00a58f
Kan-Ru Chen <kanru@kanru.info>
2026-05-18 23:09:53 +0900

    tip_host: add compatibility to the manifest

--- crates/chewing_tip_host/build.rs
+++ crates/chewing_tip_host/build.rs
@@ -5,17 +5,12 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     embedinator::ResourceBuilder::from_env()
-        .add_manifest(if cfg!(debug_assertions) {
-            std::fs::read_to_string("rc/debug.manifest")?
-        } else {
-            std::fs::read_to_string("rc/release.manifest")?
-        })
+        .add_manifest(std::fs::read_to_string("app.manifest")?)
         .add_string(
             "LegalCopyright",
             "Copyright (C) 2013-2026 libchewing Core Team",
         )
         .finish();
-    println!("cargo:rerun-if-changed=rc/debug.manifest");
-    println!("cargo:rerun-if-changed=rc/release.manifest");
+    println!("cargo:rerun-if-changed=app.manifest");
     Ok(())
 }
